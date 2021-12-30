b945872e5bb189122169649105003c22abe6ef85
Mike <mike@mike-francis.org>
2021-12-30 21:48:19 +0000

    updated html and started to develop js for simple colour switcher

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function (){
+    // get random number between 0 - 3
+    // const randomNumber = Math.floor(Math.random() * 4);
+    const randomNumber = 2;
+})
