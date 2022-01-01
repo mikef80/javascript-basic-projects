59a34ab1033cdfb786f762c309591c3d0140c90c
Mike <mike@mike-francis.org>
2022-01-01 12:17:13 +0000

    completed colour flipper project with basic JS functions

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -5,6 +5,12 @@ const color = document.querySelector('.color');
 
 btn.addEventListener('click', function (){
     // get random number between 0 - 3
-    // const randomNumber = Math.floor(Math.random() * 4);
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 })
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length);
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// ASSIGN BUTTON AND SPAN TEXT TO VARIABLES
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// ADD EVENT LISTENER FOR BUTTON CLICK AND RUN FUNCTION
+btn.addEventListener('click', function(){
+    let hexColor = '#';
+    for (let i = 0; i < 6; i ++) {
+        hexColor += hex[randomNumber()];
+    }
+    
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+// RANDOM NUMBER GENERATOR FOR HEX CODE
+const randomNumber = () => {
+    return Math.floor(Math.random() * hex.length);
+}
