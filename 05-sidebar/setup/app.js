62dd5e6f0f65b5549e24cc941abf773f955e7b9c
Mike <mike@mike-francis.org>
2022-01-02 14:08:52 +0000

    completed sidebar project

--- 05-sidebar/setup/app.js
+++ 05-sidebar/setup/app.js
@@ -4,10 +4,8 @@ const closeBtn = document.querySelector('.close-btn');
 
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('show-sidebar');
-    console.log(sidebar.classList);
 })
 
 closeBtn.addEventListener('click', function() {
     sidebar.classList.remove('show-sidebar');
-    console.log(sidebar.classList);
 })
